23baef5dd737b8414e8670a7fccd6f1e8fafe8bf
matthias paul raus <81903294+MatthiasPaulRaus@users.noreply.github.com>
2023-06-02 16:25:54 +0200

    first commit

---
+++ src/QuestionsList.jsx
@@ -0,0 +1,221 @@
+const QuestionsList = [
+    {
+        question:"Es muss sich um einen Fehler handeln.",
+        answersList: [
+            {answer:"Da bin ich mir ganz sicher.", isCorrect:true},
+            {answer:"Das kann ich mir vorstellen.", isCorrect:false},
+            {answer:"Das habe ich mir gedacht.", isCorrect:false},
+        ]
+    },{
+        question:"Auch diesmal muss es sich darum gehandelt haben.",
+        answersList: [
+            {answer:"Da hatten wir unsere Zweifel.", isCorrect:false},
+            {answer:"Da waren wir uns absolut sicher.", isCorrect:true},
+            {answer:"Das könnte so gewesen sein.", isCorrect:false},
+        ]
+    },{
+        question:"Das muss die neue Kollegin sein.",
+        answersList: [
+            {answer:"Möglicherweise hast du Recht.", isCorrect:false},
+            {answer:"Vielleicht, aber ich bin mir nicht sicher.", isCorrect:false},
+            {answer:"Daran besteht kein Zweifel.", isCorrect:true},
+        ]
+    },{
+        question:"Das kann nur Claudia sein.",
+        answersList: [
+            {answer:"Nur sie ruft noch so spät an.", isCorrect:true},
+            {answer:"Möglicherweise ist sie das.", isCorrect:false},
+            {answer:"Wahrscheinlich ruft sie dich an.", isCorrect:false},
+        ]
+    },{
+        question:"Dieser Versuch muss scheitern.",
+        answersList: [
+            {answer:"Es ist durchaus möglich, dass es klappt.", isCorrect:false},
+            {answer:"Das kann so nicht gut gehen.", isCorrect:true},
+            {answer:"Vielleicht könnte es gelingen.", isCorrect:false},
+        ]
+    },{
+        question:"Die Krankheit dürfte vor allem Kinder betroffen haben.",
+        answersList: [
+            {answer:"Die Spezialisten glauben das.", isCorrect:false},
+            {answer:"Die Forscher sind sich ganz sicher.", isCorrect:false},
+            {answer:"Die Ärzte vermuten das.", isCorrect:true},
+        ]
+    },{
+        question:"Die Hautflecken könnten von einer Allergie kommen.",
+        answersList: [
+            {answer:"Das halte ich für durchaus möglich.", isCorrect:true},
+            {answer:"Das kann ich kaum glauben.", isCorrect:false},
+            {answer:"Da bin ich mir ganz sicher.", isCorrect:false},
+        ]
+    },{
+        question:"Ein 100-Jähriger soll den Marathonlauf gewonnen haben.",
+        answersList: [
+            {answer:"Das hat der Sportler gestern behauptet.", isCorrect:false},
+            {answer:"Das wurde in den Medien berichtet.", isCorrect:true},
+            {answer:"Das habe ich selbst gesehen.", isCorrect:false},
+        ]
+    },{
+        question:"Er will durch seine Geschäfte reich geworden sein.",
+        answersList: [
+            {answer:"So stand es in der Online-Zeitung.", isCorrect:false},
+            {answer:"Davon konnte ich mich selbst überzeugen.", isCorrect:false},
+            {answer:"So hat er es im Fernsehen behauptet.", isCorrect:true},
+        ]
+    },{
+        question:"Er selbst behauptet, dass er das gemacht hat.",
+        answersList: [
+            {answer:"Er will das getan haben.", isCorrect:true},
+            {answer:"Er könnte es gewesen sein.", isCorrect:false},
+            {answer:"Er muss das gemacht haben.", isCorrect:false},
+        ]
+    },
+    
+    {
+        question:"Ihr Verhalten hat mir sehr missfallen.",
+        answersList: [
+            {answer:"Tut mir Leid, dass es Ihnen nicht gefallen hat.", isCorrect:true},
+            {answer:"Schade, aber mir hat es dort gut gefallen.", isCorrect:false},
+            {answer:"Das lasse ich mir nicht gefallen.", isCorrect:false},
+        ]
+    },{
+        question:"Sie sollten die App schnellstens deinstallieren.",
+        answersList: [
+            {answer:"Aber ich habe sie doch erst neulich installiert.", isCorrect:true},
+            {answer:"Ja, aber ich habe sie noch nicht installiert.", isCorrect:false},
+            {answer:"Ich konnte sie nicht selbst installieren.", isCorrect:false},
+        ]
+    },{
+        question:"Wenn es auch sehr teuer ist, so sollten wir es uns kaufen.",
+        answersList: [
+            {answer:"Weil es sehr teuer ist, sollten wir es kaufen.", isCorrect:false},
+            {answer:"Wir kaufen es nicht, weil es sehr teuer ist.", isCorrect:false},
+            {answer:"Obwohl es viel kostet, sollten wir es kaufen.", isCorrect:true},
+        ]
+    },{
+        question:"Wie sehr er sich auch anstrengt, er wird es nicht schaffen.",
+        answersList: [
+            {answer:"Er versucht es zwar, aber er hat keine Chance.", isCorrect:true},
+            {answer:"Wenn er sich anstrengt, kann er es schaffen.", isCorrect:false},
+            {answer:"Er hat gute Chancen zu gewinnen, obwohl er sich nicht vorbereitet hat. ", isCorrect:false},
+        ]
+    },{
+        question:"Wahrscheinlich kommen wir am Sonntagabend an.",
+        answersList: [
+            {answer:"Auch wenn der Flug Verspätung hat.", isCorrect:false},
+            {answer:"Es sei denn, der Flug hat Verspätung.", isCorrect:true},
+            {answer:"Obwohl der Abflug verspätet ist.", isCorrect:false},
+        ]
+    },{
+        question:"Außer der Chef wird krank, werden wir zum Seminar reisen.",
+        answersList: [
+            {answer:"Wir kommen, obwohl der Chef krank ist.", isCorrect:false},
+            {answer:"Weil der Chef krank ist, kommen wir ohne ihn zum Treffen.", isCorrect:false},
+            {answer:"Wenn der Chef krank ist, werden wir nicht kommen.", isCorrect:true},
+        ]
+    },{
+        question:"Ihnen stehen mehrere Modelle zur Auswahl.",
+        answersList: [
+            {answer:"Verschiedene Modelle können ausgewählt werden.", isCorrect:true},
+            {answer:"Viele Modelle stehen nicht mehr zur Verfügung.", isCorrect:false},
+            {answer:"Manche Modelle konnten nicht bereitgestellt werden.", isCorrect:false},
+        ]
+    },{
+        question:"Bei diesem Projekt sollten Sie Verluste in Kauf nehmen.",
+        answersList: [
+            {answer:"Rechnen Sie nicht damit, dass Sie etwas verlieren.", isCorrect:false},
+            {answer:"Akzeptieren Sie, dass Sie Geld verlieren können.", isCorrect:true},
+            {answer:"Wenn Sie Verluste machen, ist Ihr Projekt gescheitert.", isCorrect:false},
+        ]
+    },{
+        question:"Eine Wanderung kommt bei diesem Wetter nicht infrage.",
+        answersList: [
+            {answer:"Auch wenn das Wetter schlecht ist, machen wir die Wanderung.", isCorrect:false},
+            {answer:"Unter diesen Umständen sollten wir trotzdem wandern.", isCorrect:false},
+            {answer:"Bei schlechtem Wetter ist der Ausflug nicht möglich.", isCorrect:true},
+        ]
+    },{
+        question:"Bei unserem Treffen sollte das Problem angesprochen werden.",
+        answersList: [
+            {answer:"Wenn wir uns sehen, muss das zur Sprache kommen.", isCorrect:true},
+            {answer:"Wir könnten uns darüber unterhalten, wenn wir uns wiedersehen.", isCorrect:false},
+            {answer:"Es ist kaum denkbar, dass wir darüber sprechen.", isCorrect:false},
+        ]
+    },
+    
+
+    {
+        question:"Wir sind zu müde, als dass",
+        answersList: [
+            {answer:"wir einen Spaziergang machen könnten.", isCorrect:true},
+            {answer:"man spazieren gehen kann.", isCorrect:false},
+            {answer:"sie spazieren gehen sollten.", isCorrect:false},
+        ]
+    },{
+        question:"Die Lage ist zu komplex,",
+        answersList: [
+            {answer:"um sie schnell so erklären können.", isCorrect:false},
+            {answer:"um sie so einfach erklären zu können.", isCorrect:true},
+            {answer:"um so schnell erklärt zu sein.", isCorrect:false},
+        ]
+    },{
+        question:"Die Leute waren in der Lage sich der Umwelt anzupassen.",
+        answersList: [
+            {answer:"Sie haben in das Leben der Umwelt gepasst.", isCorrect:false},
+            {answer:"Sie wollten sich gerne integrieren.", isCorrect:false},
+            {answer:"Sie konnten ihre Lebensumstände verändern.", isCorrect:true},
+        ]
+    },{
+        question:"Versuchen Sie bei jeder Gelegenheit zu Fuß zu gehen.",
+        answersList: [
+            {answer:"Gehen Sie zu Fuß, wann immer sie können.", isCorrect:true},
+            {answer:"Gehen Sie immer zu Fuß.", isCorrect:false},
+            {answer:"Gehen Sie immer zu Fuß, wenn Sie Lust darauf haben.", isCorrect:false},
+        ]
+    },{
+        question:"Wir haben die Absicht uns demnächst mit dem Thema zu befassen.",
+        answersList: [
+            {answer:"Wir werden uns wahrscheinlich nicht bald damit beschäftigen.", isCorrect:false},
+            {answer:"Wir könnten das Thema in Betracht ziehen.", isCorrect:false},
+            {answer:"Wir wollen uns bald damit auseinandersetzen.", isCorrect:true},
+        ]
+    },{
+        question:"Es ist unumgänglich online zu reservieren.",
+        answersList: [
+            {answer:"Sie können nur online buchen.", isCorrect:true},
+            {answer:"Sie können das Museum auch ohne Reservierung besuchen.", isCorrect:false},
+            {answer:"Sie können eine Reservierung vermeiden.", isCorrect:false},
+        ]
+    },{
+        question:"Der Verzehr von mitgebrachten Speisen ist nicht gestattet.",
+        answersList: [
+            {answer:"Hier können Sie etwas in der Cafeteria essen.", isCorrect:false},
+            {answer:"Hier dürfen Sie nichts essen.", isCorrect:true},
+            {answer:"Hier sollten Sie auf Essen und Getränke verzichten.", isCorrect:false},
+        ]
+    },{
+        question:"Der Immobilienagent erwartet deinen Rückruf.",
+        answersList: [
+            {answer:"Du kannst den Agenten anrufen.", isCorrect:false},
+            {answer:"Du hast gestern auf seinen Anruf gewartet.", isCorrect:false},
+            {answer:"Du sollst den Immobilienagenten zurückrufen.", isCorrect:true},
+        ]
+    },{
+        question:"Ihr Standpunkt ist meiner Meinung nach berechtigt.",
+        answersList: [
+            {answer:"Ich kann Ihre Argumente nachvollziehen.", isCorrect:true},
+            {answer:"Sie haben ein Recht auf eine eigene Meinung.", isCorrect:false},
+            {answer:"Ich stimme nicht mit Ihrer Meinung überein.", isCorrect:false},
+        ]
+    },{
+        question:"Ihre Erklärung ist zu einfach, um das Problem zu verstehen.",
+        answersList: [
+            {answer:"Das Problem ist zu komplex, um es zu erklären.", isCorrect:false},
+            {answer:"Ihre Argumente können uns überzeugen.", isCorrect:false},
+            {answer:"Sie sollten noch auf weitere Aspekte eingehen. ", isCorrect:true},
+        ]
+    },
+
+].sort(() => Math.random() - 0.5);
+
+export default QuestionsList;
